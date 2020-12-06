c08b5d263c3537afcf8abb18e06d4fb48f397410
sara khater <sarakhater2014@gmail.com>
2020-12-06 11:45:13 +0200

    Initial Commit

---
+++ SomaliCardGames/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomaliCardGames
+//
+//  Created by unitlabs on 12/6/20.
+//  Copyright © 2020 sarakhater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
